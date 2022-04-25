912bddde6901ed0bc7902a25795c50063fcd64c5
Papillon6814 <so1rou.k@gmail.com>
2022-04-25 20:11:13 +0900

    Update basic_test.exs

--- neo4j_explayground/test/basic_test.exs
+++ neo4j_explayground/test/basic_test.exs
@@ -7,12 +7,6 @@ defmodule Neo4jExplaygroundTest.BasicTest do
     test "store works" do
       Bolt.Sips.query!(Bolt.Sips.conn(), "MATCH (n) DETACH DELETE n")
 
-      # Bolt.Sips.query!(conn, "
-      #   CREATE (m1: Match {tournament_id: 1})-[:LEFT]->(m2: Match {tournament_id: 1})-[:LEFT]->(e1: Entry {tournament_id: 1, name: 'a'})
-      #   CREATE (m2)-[:RIGHT]->(e2: Entry {tournament_id: 1, name: 'b'})
-      #   CREATE (m1)-[:RIGHT]->(e3: Entry {tournament_id: 1, name: 'c'})
-      # ")
-
       tournament_id = 1
 
       5
